08ae2a30ef0f43a384c2826d750565e25b3d1e9f
casionone <casionone@gmail.com>
2022-02-25 12:35:00 +0800

    fix test

--- linkis-computation-governance/linkis-client/linkis-cli/linkis-cli-application/src/test/java/org/apache/linkis/cli/application/LinkisClientApplicationTest.java
+++ linkis-computation-governance/linkis-client/linkis-cli/linkis-cli-application/src/test/java/org/apache/linkis/cli/application/LinkisClientApplicationTest.java
@@ -17,7 +17,7 @@
 
 package org.apache.linkis.cli.application;
 
-import org.aspectj.lang.annotation.AfterEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -121,7 +121,7 @@ public class LinkisClientApplicationTest {
                 };
     }
 
-    @After
+    @AfterEach
     public void after() throws Exception {}
 
     /** Method: main(String[] args) */
