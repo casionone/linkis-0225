a82c321dd34537fc2b4f7059b647b54c514aaef6
xiaojie19852006 <xiaojie19852006@163.com>
2022-03-14 10:10:11 +0800

    add datax engine

--- linkis-computation-governance/linkis-manager/label-common/src/main/java/org/apache/linkis/manager/label/conf/LabelCommonConfig.java
+++ linkis-computation-governance/linkis-manager/label-common/src/main/java/org/apache/linkis/manager/label/conf/LabelCommonConfig.java
@@ -5,16 +5,16 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.linkis.manager.label.conf;
 
 import org.apache.linkis.common.conf.CommonVars;
@@ -47,13 +47,7 @@ public class LabelCommonConfig {
 
     public final static CommonVars<String> APPCONN_ENGINE_VERSION = CommonVars.apply("wds.linkis.appconn.engine.version", "1");
 
-    public final static CommonVars<String> FLINK_ENGINE_VERSION = CommonVars.apply("wds.linkis.flink.engine.version", "1.11.1");
-
-    public final static CommonVars<String> SQOOP_ENGINE_VERSION = CommonVars.apply("wds.linkis.sqoop.engine.version", "1.4.6");
-
-    public final static CommonVars<String> DATAX_ENGINE_VERSION = CommonVars.apply("wds.linkis.datax.engine.version", "3.0.0");
-
-    public final static CommonVars<String> PRESTO_ENGINE_VERSION = CommonVars.apply("wds.linkis.presto.engine.version", "0.234");
+    public final static CommonVars<String> FLINK_ENGINE_VERSION = CommonVars.apply("wds.linkis.flink.engine.version", "1.12.2");
 
     public final static CommonVars<String> PERMANENT_LABEL = CommonVars.apply("wds.linkis.am.permanent.label", "tenant");
 
@@ -61,4 +55,7 @@ public class LabelCommonConfig {
 
     public final static CommonVars<String> ENGINE_CONN_SPRING_NAME = CommonVars.apply("wds.linkis.engineconn.name", "linkis-cg-engineconn");
 
+    public final static CommonVars<String> DATAX_ENGINE_VERSION = CommonVars.apply("wds.linkis.datax.engine.version", "3.0.0");
+
 }
+
--- linkis-computation-governance/linkis-manager/label-common/src/main/java/org/apache/linkis/manager/label/entity/engine/EngineType.scala
+++ linkis-computation-governance/linkis-manager/label-common/src/main/java/org/apache/linkis/manager/label/entity/engine/EngineType.scala
@@ -45,15 +45,11 @@ object EngineType extends Enumeration with Logging {
 
   val ES = Value("es")
 
-  val PRESTO = Value("presto")
+  val DATAX = Value("datax")
 
   val FLINK = Value("flink")
 
   val APPCONN = Value("appconn")
-
-  val SQOOP = Value("sqoop")
-
-  val DATAX = Value("datax")
 
   def mapFsTypeToEngineType(fsType: String): String = {
     fsType match {
@@ -81,10 +77,8 @@ object EngineType extends Enumeration with Logging {
     case _ if IO_ENGINE_HDFS.toString.equalsIgnoreCase(str) => IO_ENGINE_HDFS
     case _ if PIPELINE.toString.equalsIgnoreCase(str) => PIPELINE
     case _ if ES.toString.equalsIgnoreCase(str) => ES
-    case _ if PRESTO.toString.equalsIgnoreCase(str) => PRESTO
     case _ if FLINK.toString.equalsIgnoreCase(str) => FLINK
     case _ if APPCONN.toString.equals(str) => APPCONN
-    case _ if SQOOP.toString.equalsIgnoreCase(str) => SQOOP
     case _ if DATAX.toString.equalsIgnoreCase(str) => DATAX
     case _ => null
 
